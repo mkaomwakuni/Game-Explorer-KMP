1f4e94feacf1ffea2ca365e4a1489cda9d04dc1c
mkaomwakuni <mkaoibrahim@gmail.com>
2025-04-22 09:15:00 +0300

    Add Koin initialization to iOS and Desktop platforms

--- composeApp/src/iosMain/kotlin/org/sea/rawg/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/sea/rawg/MainViewController.kt
@@ -1,5 +1,10 @@
 package org.sea.rawg
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.sea.rawg.di.AppModule
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    // Initialize Koin
+    AppModule.init()
+    App()
+}
