547d97337692de7d8186ef5f688af70deff413fc
mkaomwakuni <mkaoibrahim@gmail.com>
2025-04-23 16:20:00 +0300

    Add Koin initialization to JVM and WASM JS platforms

--- composeApp/src/jvmMain/kotlin/org/sea/rawg/main.kt
+++ composeApp/src/jvmMain/kotlin/org/sea/rawg/main.kt
@@ -2,12 +2,18 @@ package org.sea.rawg
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.sea.rawg.di.AppModule
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "KMP-Game Explorer",
-    ) {
-        App()
+fun main() {
+    // Initialize Koin
+    AppModule.init()
+
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "KMP-Game Explorer",
+        ) {
+            App()
+        }
     }
 }
--- composeApp/src/wasmJsMain/kotlin/org/sea/rawg/Main.kt
+++ composeApp/src/wasmJsMain/kotlin/org/sea/rawg/Main.kt
@@ -2,18 +2,22 @@ package org.sea.rawg
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
-import moe.tlaster.precompose.PreCompose
-import org.sea.rawg.theme.GameExplorerTheme
+import moe.tlaster.precompose.PreComposeApp
+import org.sea.rawg.di.AppModule
+import org.sea.rawg.theme.RAWGTheme
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Suppress("DEPRECATION")
 fun main() {
+    // Initialize Koin
+    AppModule.init()
+
     // Use CanvasBasedWindow despite deprecation warnings - this is a temporary solution
     // until we properly implement the newer APIs
     CanvasBasedWindow("KMP-Game Explorer") {
         // Initialize PreCompose for web
-        PreCompose {
-            GameExplorerTheme {
+        PreComposeApp {
+            RAWGTheme {
                 App()
             }
         }
